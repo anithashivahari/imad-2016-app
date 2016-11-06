7e58e7ad6b75720940c3dd108011a8649eabb5fd
anithashivahari <anitha.sugumar@gmail.com>
2016-11-06 19:43:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req,res){
-    res.send('article one will be served sbortly');
+    res.send('article one requested and will be served sbortly');
     
 });
 app.get('/article-two',function(req,res){
-    res.send('article two will be served sbortly');
+    res.send('article two requested and will be served sbortly');
     
 });
 app.get('/article-three',function(req,res){
-    res.send('article three will be served sbortly');
+    res.send('article three requested and will be served sbortly');
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
