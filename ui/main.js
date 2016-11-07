191c23c06182fd2d74665a2d99ea047421ccfb7c
anithashivahari <anitha.sugumar@gmail.com>
2016-11-07 16:10:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,14 +14,21 @@ button.onclick=function()
         if(request.readyState === XMLHttpRequest.DONE){
             //take some action
             if(request.status ===200){
-               var counter= request.responseText;
-                var span= document.getElementById('count');
-    span.innerHTML = counter.toString();
+                 var names=request.responseText;
+                 name=JSON.parse(names);
+    var list='';
+    for(var i=0;i<names.length;i++)
+    {
+        list+='<li>'+names[i]+'</li>';
+    }
+    var ul=document.getElementById('namelist');
+    ul.innerHTML=list;
+               
             }
         }
     }; 
     //make a request
-    request.open('GET','http://anithashivahari.imad.hasura-app.io/counter',true);
+    request.open('GET','http://anithashivahari.imad.hasura-app.io/submit-name?name='+name,true);
     request.send(null);
     // render the variable in the coorect 
    
@@ -33,14 +40,7 @@ var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
     //make a request to server and send name
     //capture list of name and renders list
-    var names=['name1','name2','nane3','name4'];
-    var list='';
-    for(var i=0;i<names.length;i++)
-    {
-        list+='<li>'+names[i]+'</li>';
-    }
-    var ul=document.getElementById('namelist');
-    ul.innerHTML=list;
+   
 };
 
     
