0e83081c63cda30cdb43ecbcf87313dd65a55aec
anithashivahari <anitha.sugumar@gmail.com>
2016-11-07 06:31:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -100,6 +100,10 @@ app.get('/:articleName',function(req,res){
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
