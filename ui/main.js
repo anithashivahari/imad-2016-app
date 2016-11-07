eee08de301de1ed3df8d040dfd987e366d0b3def
anithashivahari <anitha.sugumar@gmail.com>
2016-11-07 06:49:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,9 @@ console.log('Loaded!');
 //change the text of main text file
 var element=document.getElementById('main-text');
 element.innerHTML = 'New value';
+//move the image
+var img=document.getElementById('img');
+img.onClick = function()
+{
+    img.style.marginleft='100px';
+}
