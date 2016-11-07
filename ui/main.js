921c17760cf826adfcf54fa0a38bbfbd31350674
anithashivahari <anitha.sugumar@gmail.com>
2016-11-07 16:29:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,7 +28,7 @@ button.onclick=function()
         }
     }; 
     //make a request
-    request.open('GET','http://anithashivahari.imad.hasura-app.io/submit-name?name='+name,true);
+    request.open('GET','http://anithashivahari.imad.hasura-app.io/counter',true);
     request.send(null);
     // render the variable in the coorect 
    
@@ -38,6 +38,31 @@ var nameInput=document.getElementById('name');
 var name=nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
+    var request=new XMLHttpRequest();
+    
+    
+    //capture response and store in a variable
+    
+    request.onreadystatechange=function() {
+        if(request.readyState === XMLHttpRequest.DONE){
+            //take some action
+            if(request.status ===200){
+                
+                 var names=request.responseText;
+                 name=JSON.parse(names);
+    var list='';
+    for(var i=0;i<names.length;i++)
+    {
+        list+='<li>'+names[i]+'</li>';
+    }
+    var ul=document.getElementById('namelist');
+    ul.innerHTML=list;
+            }
+        }
+    };
+     request.open('GET','http://anithashivahari.imad.hasura-app.io/submit-name?name='+name,true);
+    request.send(null);
+    
     //make a request to server and send name
     //capture list of name and renders list
    
