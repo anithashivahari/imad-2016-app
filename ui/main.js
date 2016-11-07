a499f4f9d8b1ef7b14930f024a647f49e163b437
anithashivahari <anitha.sugumar@gmail.com>
2016-11-07 07:42:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,11 +7,11 @@ var img=document.getElementById('madi');
 var marginLeft=0;
 function moveRight()
 {
-    marginLeft = marginLeft + 1;
+    marginLeft = marginLeft + 10;
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onClick = function()
 {
-    var interval = setInterval(moveRight,50);
+    var interval = setInterval(moveRight,100);
     
 };
