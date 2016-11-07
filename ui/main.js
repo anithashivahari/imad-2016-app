79a583353c0635eb226c57cb73394e8744043143
anithashivahari <anitha.sugumar@gmail.com>
2016-11-07 13:32:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,7 +16,7 @@ button.onclick=function()
             if(request.status ===200){
                var counter= request.responseText;
                 var span= document.getElementById('count');
-    span.InnerHTML = counter.toString();
+    span.innerHTML = counter.toString();
             }
         }
     }; 
